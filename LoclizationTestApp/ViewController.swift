9ee2c1e6cda2231e2d20d19a8203e62a8959f0df
Yurij Alt <yura_chekalyuk@mail.ru>
2022-02-18 22:57:52 +0300

    Localized

--- LoclizationTestApp/ViewController.swift
+++ LoclizationTestApp/ViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        label.text = NSLocalizedString("text_exp", comment: "")
     }
-
-
 }
 
